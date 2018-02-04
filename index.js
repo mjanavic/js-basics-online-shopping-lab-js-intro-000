12c85cd333d671fb15ef1b2d8ecb19fcc65ff244
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-02-04 17:33:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ function viewCart() {
       if (cart.length === 1) {
         result += `${Object.keys(cart[i])} at $${Object.values(cart[i])}.`;
       } else {
-        if (i < (cart.length - 1)) {
-          result += `${Object.keys(cart[i])} at $${Object.values(cart[i])} and `;
-        } else if (i === (cart.length - 1)) {
+        if (i === cart.length) {
           result += `${Object.keys(cart[i])} at $${Object.values(cart[i])}.`;
+        } else if (i === (cart.length - 1)) {
+          result += `${Object.keys(cart[i])} at $${Object.values(cart[i])} and`;
         } else {
           result += `${Object.keys(cart[i])} at $${Object.values(cart[i])} ,`;
         }
