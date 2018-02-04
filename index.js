5e504f00c9e60504adbac3b09178957c4dd83a1c
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-02-04 18:47:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,10 +60,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  var cost = total();
   if (cardNumber === undefined) {
     console.log('Sorry, we don\'t have a credit card on file for you.');
   } else {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}`);
+    console.log(`Your total cost is $${cost}, which will be charged to the card ${cardNumber}`);
   }
   cart = [];
 }
